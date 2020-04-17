32296e50c329ee99cd1d3c01655fc15dfddf2c73
Andrew Trettel <atrettel@users.noreply.github.com>
2020-04-16 20:55:44 -0400

    feat: move constants to wbbase module

---
+++ src/wbbase.f90
@@ -0,0 +1,8 @@
+! Copyright (C) 2020 Andrew Trettel
+module wbbase
+   implicit none
+
+   integer, parameter :: MPI_MASTER = 0
+   integer, parameter :: EXIT_FAILURE = 1
+   integer, parameter :: EXIT_SUCCESS = 0
+end module wbbase
--- src/windbag.f90
+++ src/windbag.f90
@@ -1,29 +1,28 @@
-! Copyright (C) 2020 Andrew Trettel.  All rights reserved.
+! Copyright (C) 2020 Andrew Trettel
 program windbag
    use iso_fortran_env
    use mpi
+   use wbbase
 
    implicit none
    character(len=64) :: filename
-   integer :: argc, filename_length, ierr, mpi_rank, mpi_master
+   integer :: argc, filename_length, ierr, mpi_rank
    logical :: file_exists
-
-   mpi_master = 0
 
    call mpi_init( ierr )
    call mpi_comm_rank( mpi_comm_world, mpi_rank, ierr )
-   if ( mpi_rank .eq. mpi_master ) then
+   if ( mpi_rank .eq. MPI_MASTER ) then
       argc = command_argument_count()
       if ( argc .eq. 0 ) then
          write (*,"(A)") "Usage: windbag [INPUT_FILE]"
-         call mpi_abort( mpi_comm_world, 0, ierr )
+         call mpi_abort( mpi_comm_world, EXIT_SUCCESS, ierr )
       end if
 
       call get_command_argument( 1, filename, filename_length, ierr )
       inquire( file=filename, exist=file_exists, iostat=ierr )
       if ( file_exists .eqv. .false. ) then
          write (*,"(A)") "windbag: input file does not exist"
-         call mpi_abort( mpi_comm_world, 1, ierr )
+         call mpi_abort( mpi_comm_world, EXIT_FAILURE, ierr )
       end if
    end if
 
