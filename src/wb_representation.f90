f34eae30d307fd2ee5b0c5449db3404162a17de5
Andrew Trettel <atrettel@users.noreply.github.com>
2020-05-26 15:24:03 -0400

    feat: add functions to convert precisions to strings

--- src/wb_representation.f90
+++ src/wb_representation.f90
@@ -13,13 +13,13 @@
 ! Windbag.  If not, see <https://www.gnu.org/licenses/>.
 module wb_representation
    use iso_fortran_env, only : int32, int64, real32, real64
-   use mpi_f08, only : mpi_sizeof, mpi_type_match_size, MPI_Datatype, &
-      MPI_TYPECLASS_INTEGER, MPI_TYPECLASS_REAL
+   use mpi_f08
    implicit none
 
    private
 
-   public find_mpi_precisions
+   public find_mpi_precisions, mpi_integer_precision, mpi_real_precision, &
+      fortran_integer_precision, fortran_real_precision
 
    integer, public, parameter       ::     FP = real64
    type(MPI_Datatype), public, save :: MPI_FP
@@ -56,4 +56,60 @@ contains
       call mpi_sizeof( 1_MP, mpi_size, ierr )
       call mpi_type_match_size( MPI_TYPECLASS_INTEGER, mpi_size, MPI_MP, ierr )
    end subroutine find_mpi_precisions
+
+   function fortran_integer_precision( vp ) result( string )
+      integer, intent(in) :: vp
+      character(len=STRING_LENGTH) :: string
+
+      if ( vp .eq. int64 ) then
+         write (string,"(A)") "`int64`"
+      else if ( vp .eq. int32 ) then
+         write (string,"(A)") "`int32`"
+      else
+         write (string,"(A)") "(unknown)"
+      end if
+   end function fortran_integer_precision
+
+   function fortran_real_precision( vp ) result( string )
+      integer, intent(in) :: vp
+      character(len=STRING_LENGTH) :: string
+
+      if ( vp .eq. real64 ) then
+         write (string,"(A)") "`real64`"
+      else if ( vp .eq. real32 ) then
+         write (string,"(A)") "`real32`"
+      else
+         write (string,"(A)") "(unknown)"
+      end if
+   end function fortran_real_precision
+
+   function mpi_integer_precision( datatype ) result( string )
+      type(MPI_Datatype), intent(in) :: datatype
+      character(len=STRING_LENGTH) :: string
+
+      if ( datatype .eq. MPI_INTEGER8 ) then
+         write (string,"(A)") "`MPI_INTEGER8`"
+      else if ( MPI_MP .eq. MPI_INTEGER4 ) then
+         write (string,"(A)") "`MPI_INTEGER4`"
+      else if ( MPI_MP .eq. MPI_INTEGER ) then
+         write (string,"(A)") "`MPI_INTEGER`"
+      else
+         write (string,"(A)") "(unknown)"
+      end if
+   end function mpi_integer_precision
+
+   function mpi_real_precision( datatype ) result( string )
+      type(MPI_Datatype), intent(in) :: datatype
+      character(len=STRING_LENGTH) :: string
+
+      if ( datatype .eq. MPI_REAL8 ) then
+         write (string,"(A)") "`MPI_REAL8`"
+      else if ( MPI_MP .eq. MPI_REAL4 ) then
+         write (string,"(A)") "`MPI_REAL4`"
+      else if ( MPI_MP .eq. MPI_REAL ) then
+         write (string,"(A)") "`MPI_REAL`"
+      else
+         write (string,"(A)") "(unknown)"
+      end if
+   end function mpi_real_precision
 end module wb_representation
